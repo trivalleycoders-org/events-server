3b3e367f6ff4f58d4c9448a1dbc28a8a8f880d3a
Hemalatha Manickam <hmanicka@gmail.com>
2018-07-01 12:24:01 -0700

    removed get route

--- routes/image-route.js
+++ routes/image-route.js
@@ -17,15 +17,15 @@ router.post('/', async (req, res) => {
     form.multiples = true
     form.uploadDir = path.join(__dirname, '/uploads')
 
-    form.on('file', function(field, file) {
+    form.on('file', function (field, file) {
       red('** form.on.file')
       const newFileName = path.join(form.uploadDir, file.name)
       fs.rename(file.path, newFileName, function () {
         fs.readFile(newFileName, (err, data) => {
           if (err) throw err
           const s3 = new S3()
-          const params = {Bucket: bucketName, Key: file.name, Body: data}
-          s3.upload(params, function(err, data) {
+          const params = { Bucket: bucketName, Key: file.name, Body: data }
+          s3.upload(params, function (err, data) {
             console.log('done', err, data)
             const ret = pick(['Location', 'Key'], data)
             res.send(ret)
@@ -34,29 +34,19 @@ router.post('/', async (req, res) => {
 
       })
     })
-    form.on('error', function(err) {
+    form.on('error', function (err) {
       red('** form.on.error')
       console.log('An error has occured: \n' + err)
     })
-    form.on('end', function() {
+    form.on('end', function () {
       red('** form.on.end')
       // res.status(200).send({"message": "done"})
     })
     form.parse(req)
-  } catch(e) {
+  } catch (e) {
     // red('events.route: post', e)
     red('error', e)
     res.status(400).send(e)
-  }
-})
-
-router.get('/test', (req, res) => {
-  try {
-    const b = req.body
-    yellow('b', b)
-    res.send({"message": 'hello from GET /images/test'})
-  } catch (e) {
-    red('get ERROR', e)
   }
 })
 
