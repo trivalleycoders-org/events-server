65d0ddc4cae72047fbd7c1e2827c518f68ecb87c
klequis <klequis@gmail.com>
2018-08-06 17:17:29 -0700

    fixed to match new shape

--- test/events-route/post/fixture.js
+++ test/events-route/post/fixture.js
@@ -6,11 +6,7 @@ export const eventToPost = {
   imageUrl: 'https://s3-us-west-2.amazonaws.com/photo-app-tvc/briia.jpg',
   linkToUrl: 'http://briia.io',
   organization: 'BRIIA',
-  postalCode: {
-    _id: '5b5f6f52222be42bb919c008',
-    postalCode: '94582',
-    searchString: '94582 San Ramon California'
-  },
+  postalCode_id: '5b5f6f52222be42bb919c008',
   price: 75,
   stateCode: 'CA',
   startDateTime: '2018-06-12T16:00:00.000Z',
@@ -35,43 +31,4 @@ export const eventAfter = {
   tags: [ 'health' ],
   title: 'BRIIA Investor Demo Day',
   venueName: 'Dublin Concert Hall',
-}
-
-
-
-// Shape out example -----------------------------------
-
-// const shapeOut = {
-//   endDateTime: '2018-08-06T17:13:56.101Z',
-//   startDateTime: '2018-08-06T17:13:56.088Z',
-//   title: 'test',
-//   organization: 'org',
-//   venueName: 'ven',
-//   postalCode: {
-//     _id: '5b5f6f52222be42bb919c008',
-//     postalCode: '94582',
-//     searchString: '94582 San Ramon California'
-//   },
-//   linkToUrl: 'link',
-//   price: '30',
-//   category: 'octocopter',
-//   tags: [
-//     'one'
-//   ]
-// }
-
-// -----------------------------------------------
-
-const shapeOut = {
-  postalCode: {
-    _id: '5b5f6f52222be42bb919c008',
-    postalCode: '94582',
-    searchString: '94582 San Ramon California'
-  },
-}
-
-const inDB = {
-  cityName: 'San Ramon',
-  postalCode: '94582',
-  stateCode: 'CA',
 }
