bdc0cc04998fae3356959284931443e8a0a0e0e6
Hemalatha Manickam <hmanicka@gmail.com>
2018-08-18 08:24:33 -0700

    reorganized tests

--- test/dbFunctions/dbFunctions.test.js
+++ test/dbFunctions/dbFunctions.test.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import { expect } from 'chai'
 import { yellow, blue, green, red, greenf, redf } from '../../logger/'
-import { people } from './fixture'
+import { people, person } from './fixture'
 import {
   find,
   findById,
@@ -40,7 +40,6 @@ before(() => {
 })
 
 after(() => {
-  // dropPeopleCollection()
   dropAllCollections()
   if (!process.env.WATCH) {
     setTimeoutPromise(1900).then((value) => {
@@ -49,108 +48,57 @@ after(() => {
   }
 })
 
-describe('find()', () => {
+describe('dbFunctions', () => {
   before(() => {
     dropPeopleCollection()
   })
-  it.only('should find 4 people', async () => {
+
+  let pid
+
+  it('should add more than one person', async () => {
     const insert = await insertMany('people', people)
-    const d1 = insert.data
-    expect(d1.length).to.equal(4)
+    const data = insert.data
+    expect(data.length).to.equal(4)
+  })
+
+  it('should find 4 people', async () => {
     const f = await find('people')
-    // console.log('f: ', f)
+    const fdata = f.data
     expect(f.data.length).to.equal(4)
   })
-})
 
-describe('findById()', () => {
-  before(() => {
-    dropPeopleCollection()
+  it('should add one person', async () => {
+    const insert = await insertOne('people', person)
+    const data = insert.data
+    expect(data.length).to.equal(1)
+    const p = data[0]
+    pid = data[0]._id
+    expect(p.first).to.equal('John')
   })
-  it('should find by id', () => {
-    const insert = insertOne('people', people[0])
-    const d1 = insert.data
-    expect(d1.length).to.equal(1)
-    const id = d1[0]._id
-    const f = findById('people', id)
+
+  it('should find by id', async () => {
+    const f = await findById('people', pid)
     const data = f.data
     expect(data.length).to.equal(1)
     const p = data[0]
-    expect(p.first).to.equal('Abe')
+    expect(p.first).to.equal('John')
   })
-})
 
-describe('findOneAndDelete()', () => {
-  before(async () => {
-    dropPeopleCollection()
-  })
-  it('should find by id and delete', async () => {
-    const insert = await insertMany('people', people)
-    const d1 = insert.data
-    expect(d1.length).to.equal(4)
-    const id = d1[0]._id
-    const f = await findOneAndDelete('people', id)
-    const data = f.data
-    expect(data.length).to.equal(1)
-    const p = data[0]
-    expect(p.first).to.equal('Abe')
-  })
-})
-
-describe('findOneAndUpdate()', () => {
-  before(async () => {
-    dropPeopleCollection()
-  })
   it('should find by id and update', async () => {
-    const insert = await insertMany('people', people)
-    const insertedData = insert.data
-    expect(insertedData.length).to.equal(4)
-    const id = insertedData[0]._id
-    const newData1 = { first: 'Jed' }
-    const update1 = await findOneAndUpdate('people', id, newData1)
-    const data1 = update1.data
-    expect(data1.length).to.equal(1)
-    const person1 = data1[0]
-    expect(person1.first).to.equal('Jed')
-    //
-    const newData2 = { last: 'Jenkins' }
-    const update2 = await findOneAndUpdate('people', id, newData2)
-    const data2 = update2.data
-    expect(data2.length).to.equal(1)
-    const person2 = data2[0]
-    expect(person2.last).to.equal('Jenkins')
-    //
     const newData3 = { first: 'Bob', last: 'Bradcliff' }
-    const update3 = await findOneAndUpdate('people', id, newData3)
+    const update3 = await findOneAndUpdate('people', pid, newData3)
     const data3 = update3.data
     expect(data3.length).to.equal(1)
     const person3 = data3[0]
     expect(person3.first).to.equal('Bob')
     expect(person3.last).to.equal('Bradcliff')
   })
-})
 
-describe('insert()', () => {
-  before(async () => {
-    dropPeopleCollection()
-  })
-  it('should add one person', async () => {
-    const onePerson = people[0]
-    const insert = await insertOne('people', onePerson)
-    const data = insert.data
+  it('should find by id and delete', async () => {
+    const f = await findOneAndDelete('people', pid)
+    const data = f.data
     expect(data.length).to.equal(1)
     const p = data[0]
-    expect(p.first).to.equal('Abe')
-  })
-})
-
-describe('insertMany()', () => {
-  before(async () => {
-    dropPeopleCollection()
-  })
-  it('should add one person', async () => {
-    const insert = await insertMany('people', people)
-    const data = insert.data
-    expect(data.length).to.equal(4)
+    expect(p.first).to.equal('Bob')
   })
 })
