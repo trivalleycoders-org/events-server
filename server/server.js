f7ea06f2e76b663b21f47b3286304664f0c7f4fd
Hemalatha Manickam <hmanicka@gmail.com>
2018-10-01 17:42:18 -0700

    serve client files from server

--- server/server.js
+++ server/server.js
@@ -14,6 +14,21 @@ import location from '../routes/location-route'
 import starwars from '../routes/starwars-route'
 
 const app = express()
+const path = require('path')
+
+// source: https://daveceddia.com/deploy-react-express-app-heroku/
+
+// Serve static files from the React app
+const pathToStaticFiles = path.resolve(__dirname, '../client/build')
+console.log('static-root:', pathToStaticFiles)
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(pathToStaticFiles, 'index.html'))
+// })
+
 const port = process.env.PORT
 
 if (process.env.NODE_ENV !== 'production') {
